44d3ca268c711776759fd616aa82638ed3083ad9
owaisbukhari00 <owaisbukhari@outlook.com>
2022-02-07 22:55:22 +0800

    solved problem using <algorithm>

--- ENTRYTEST/main.cpp
+++ ENTRYTEST/main.cpp
@@ -1,35 +1,21 @@
-#include <cinttypes>
 #include<iostream>
+#include<algorithm>
 #include<string>
 
 int rearrange(int);
 
 uint64_t descendingOrder(uint64_t a)
 {
-    std::string temp, temp2;
-    int max, maxindex;
-
-    temp = std::to_string(a);
-  
-    for(int i=0; i<temp.size();i++)
-    {
-        char ch  = temp[i];
-        for(int j=0; j<temp.size();j++)
-        {
-            if (temp[j] > temp[i])
-            {
-                ch = temp[j];
-                maxindex = j;
-            }
-        }
-        temp2.push_back(ch);
-        temp[maxindex] = 0;
-    }
-    return stoi(temp2);
+    std::string temp = std::to_string(a);
+    std::sort(temp.begin(), temp.end());
+    std::reverse(temp.begin(), temp.end());   
+    std::cout << temp;
+    
+    return stoull(temp);
 }
 
 int main()
 {
-    std::cout << descendingOrder(1234);
+    std::cout << descendingOrder(52347);
     return 0;
 }
