ae98207c687dbd560a49e38cd047d9a000b8a2e8
owaisbukhari00 <owaisbukhari@outlook.com>
2022-02-07 23:02:24 +0800

    cleaned file

--- ENTRYTEST/main.cpp
+++ ENTRYTEST/main.cpp
@@ -2,15 +2,13 @@
 #include<algorithm>
 #include<string>
 
-int rearrange(int);
-
 uint64_t descendingOrder(uint64_t a)
 {
     std::string temp = std::to_string(a);
+
     std::sort(temp.begin(), temp.end());
     std::reverse(temp.begin(), temp.end());   
-    std::cout << temp;
-    
+
     return stoull(temp);
 }
 
