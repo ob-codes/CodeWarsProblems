7e615ff4ce05b5b67bf65eeb4ad46f32b4a51bff
owaisbukhari00 <owaisbukhari@outlook.com>
2023-04-11 00:32:49 +0500

    codewars 7Kyu

---
+++ Anagram Detection/main.js
@@ -0,0 +1,10 @@
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  let result = false;
+
+
+  return result;
+};
+
+console.log(isAnagram("foefet", "toffee")); //true
+console.log(isAnagram("apple", "pale")); //false
