82e9236c73087be24de37bd0022308467920c071
owaisbukhari00 <owaisbukhari@outlook.com>
2023-04-11 00:33:06 +0500

    [Final] codewars 7Kyu

--- Anagram Detection/main.js
+++ Anagram Detection/main.js
@@ -2,6 +2,11 @@
 var isAnagram = function(test, original) {
   let result = false;
 
+  let testArr = test.toLowerCase().split("").sort().join("");
+  let orgArr = original.toLowerCase().split("").sort().join("");
+
+  if (testArr === orgArr)
+    result = true;
 
   return result;
 };
