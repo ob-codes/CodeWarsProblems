395781756da16846484bc651dce22ce4f5d26e3c
owaisbukhari00 <owaisbukhari@outlook.com>
2023-05-30 22:37:22 +0500

    codeWar 8 Kyu

---
+++ Are You Playing Banjo/main.js
@@ -0,0 +1,8 @@
+function areYouPlayingBanjo(name) {
+  if (name.charAt(0) === "R" || name.charAt(0) === "r") 
+    return `${name} plays banjo`;
+  else 
+    return `${name} does not play banjo`;
+}
+
+console.log(areYouPlayingBanjo("Adam"));
