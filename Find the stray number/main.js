895b1e001fd1f7304d14193b7e29b1e0e43026fd
owaisbukhari00 <owaisbukhari@outlook.com>
2023-06-13 00:31:16 +0500

    [final] codeWars 7Kyu

--- Find the stray number/main.js
+++ Find the stray number/main.js
@@ -1,10 +1,17 @@
-function xx(arr){
+function stray(arr){
   let result=0;
   result = arr.sort((a, b) => a-b);
   
+  let firstElement = result.shift();
+  let lastElement = result.pop();
+  let secondLastElement = result.pop();
   
+  if (lastElement !== firstElement && lastElement !== secondLastElement)
+    result=lastElement;
+  else
+    result=firstElement;
 
   return result;
 }
 
-console.log(xx([1, 1, 2]));
+console.log(stray([1, 1, 2]));
