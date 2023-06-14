eb8e262879913ab0143f016a5a2be0b8b53eca66
owaisbukhari00 <owaisbukhari@outlook.com>
2023-06-14 23:55:29 +0500

    codeWars 7Kyu

--- Sort array by string length/main.js
+++ Sort array by string length/main.js
@@ -1,5 +1,7 @@
 function sortByLength(arr){
+  let sortedArray = arr.sort((a, b) => a.length - b.length);
   
+  return sortedArray;
 }
 
 console.log(xx(["Telescopes", "Glasses", "Eyes", "Monocles"]));
