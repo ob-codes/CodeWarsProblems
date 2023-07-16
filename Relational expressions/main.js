9c25360b295a61d1030e27a59711430353e677f5
owaisbukhari00 <owaisbukhari@outlook.com>
2023-07-16 17:36:03 +0500

    working solution. Failing some test cases

--- Relational expressions/main.js
+++ Relational expressions/main.js
@@ -5,43 +5,70 @@ function Relational(value) {
 
 Relational.prototype = {
   passed: function() {
-    this.temp = true;
-    return this.temp;
+    this.value = this.temp;
+    return this.value;
   },
 
   equals: function(input) {
     if (this.value === input) 
       this.temp = true;
-    return `${this.value} = ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `= ${input}`;
+    return this;
   },
   notEquals: function(input) {
     if (this.value != input) 
       this.temp = true;
-    return `${this.value} != ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `!= ${input}`;
+    return this;
   },
 
   lessThan: function(input) {
     if (this.value < input) 
       this.temp = true;
-    return `${this.value} < ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `< ${input}`;
+    return this;
   },
   lessThanOrEqualTo: function(input) {
     if (this.value <= input) 
       this.temp = true;
-    return `${this.value} <= ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `<= ${input}`;
+    return this;
   },
 
   greaterThan: function(input) {
     if (this.value > input) 
       this.temp = true;
-    return `${this.value} > ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `> ${input}`;
+    return this;
   },
   greaterThanOrEqualTo: function(input) {
     if (this.value >= input) 
       this.temp = true;
-    return `${this.value} >= ${input}`;
+    else
+      this.temp = false;
+
+    this.value += `>= ${input}`;
+    return this;
   }
 }
 
 let tp = new Relational(1).lessThan(2).lessThan(3).passed();
 console.log(tp);
+//10 != 5 != 5:
+let tp2 = new Relational(10).notEquals(5).notEquals(5).passed();
+console.log(tp2);
