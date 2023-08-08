6b5502ff61bd568b9606076c357e924565364a10
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-08 17:59:34 +0500

    codeWars 6Kyu

---
+++ Extract last names of people named Michael/main.js
@@ -0,0 +1,10 @@
+function xx(str){
+  let result = [];
+  
+  
+   
+  return result;
+}
+const inputText = "Michael Bor, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+
+console.log(xx(inputText));
