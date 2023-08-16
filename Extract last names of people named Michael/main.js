890b48a7f3e875f711b573e244e15cbfd8839d45
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-16 22:38:59 +0500

    [Final] codeWars 6Kyu

--- Extract last names of people named Michael/main.js
+++ Extract last names of people named Michael/main.js
@@ -1,10 +1,25 @@
-function xx(str){
+function getMichaelLastName(str){
   let result = [];
   
+  let arr = str.split(" ");
   
+  arr.forEach((element, idx) => {
+    if (element === "Michael"){
+      let nextElementFirstChar = arr[idx+1].charAt(0);
+
+      if (nextElementFirstChar >= 'A' && nextElementFirstChar <= 'Z') {  //check if its capital
+        let nextElementLastChar = arr[idx +1].charAt(arr[idx +1].length -1);
+        
+        if (nextElementLastChar < 'a' || nextElementLastChar > 'z') {
+          arr[idx +1] = arr[idx +1].slice(0, -1);
+        }
+        result.push(arr[idx+1]);
+      }        
+    }
+  });
    
   return result;
 }
 const inputText = "Michael Bor, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
 
-console.log(xx(inputText));
+console.log(getMichaelLastName(inputText));
