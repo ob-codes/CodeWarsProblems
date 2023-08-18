f4ef152df75d9ec3e1bc9b6a5d0fae2698144365
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-18 16:32:29 +0500

    codeWars 6Kyu

--- Simple Encryption #1 - Alternating Split/main.js
+++ Simple Encryption #1 - Alternating Split/main.js
@@ -1,7 +1,22 @@
 function encrypt(str, times) {
-  let result = "";
-  
-  return result;
+  if (str === "" || str === null || times <= 0)
+  return str;
+
+  while (times) {
+    let temp1 = "", temp2 = "";
+    times--;
+    str.split("")
+      .forEach((element, idx) => {
+        if (idx % 2 !== 0){
+          temp1 += element
+        }
+        else{
+          temp2 += element;
+        }
+      });
+    str = temp1 + temp2;
+  }
+
+  return str;
 }
 
-console.log(encrypt("012345", 1));
