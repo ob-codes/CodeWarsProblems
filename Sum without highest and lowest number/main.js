9e3a1fc28d2855c1c687c8a02054fa7c4c83e0c1
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-23 23:05:31 +0500

    [Final] codeWars 8Kyu

--- Sum without highest and lowest number/main.js
+++ Sum without highest and lowest number/main.js
@@ -1,6 +1,13 @@
 function sumArray(array) {
   let result = 0;
 
+  if (!Array.isArray(array) || array === null || array === [] || array.length<2)
+    return 0;
+  
+  array.sort((a, b) => a-b).pop();
+  array.shift();
+  
+  result = array.reduce((current, acc) => current + acc, 0);
 
   return result;
 }
