a77dcb1e19d75a2c79a408ac72cbad49e7ac67a5
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-28 23:51:51 +0500

    [Final] codeWars 7Kyu

--- Remove anchor from URL/main.js
+++ Remove anchor from URL/main.js
@@ -1,3 +1,15 @@
 function removeUrlAnchor(url){
-  // TODO: complete
+  let result = "";
+
+  let temp = -1;
+  temp = url.indexOf('#');
+  
+  if (temp > -1)
+    result = url.split("").splice(0, temp).join("");
+  else
+    result = url;
+
+  return result;
 }
+
+console.log(removeUrlAnchor('www.codewars.com#about'));
