ca070295b4acb2c07b1191d4b7502caaf6be8777
owaisbukhari00 <owaisbukhari@outlook.com>
2023-08-30 00:19:06 +0500

    codeWars 7Kyu

---
+++ You're a square/main.js
@@ -0,0 +1,11 @@
+var isSquare = function(n){
+  let result = false;
+  
+
+  return result;
+}
+
+console.log(isSquare(4));
+console.log(isSquare(165));
+console.log(isSquare(15));
+console.log(isSquare(9));
