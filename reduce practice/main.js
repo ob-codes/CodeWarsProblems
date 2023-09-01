a0d94e9053744388499a636e30148657b247baf1
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-01 20:49:43 +0500

    .reduce() exercise

---
+++ reduce practice/main.js
@@ -0,0 +1,15 @@
+const users  = [
+  {firstName : 'akhsay', lastName : 'saini', age: 26},
+  {firstName : 'donald', lastName : 'trump', age: 50},
+  {firstName : 'elon', lastName : 'musk', age: 75},
+  {firstName : 'deepika', 'lastName' : 'padukone', age: 26}
+];
+
+let output = users.reduce((acc, curr) => {
+  if (curr.age === 26) {
+    acc.push(curr.firstName);
+  }
+  return acc;
+}, [])
+
+console.log(output);
