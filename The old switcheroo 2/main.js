778070ee3045aef9044de6572a57682c716d9288
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-10 11:54:34 +0500

    [Final] codewars 7Kyu

--- The old switcheroo 2/main.js
+++ The old switcheroo 2/main.js
@@ -1,7 +1,22 @@
 function encode(str) {
   let result = "";
+  let alphabetsSmall = 'abcdefghijklmnopqrstuvwxyz';
+  let alphabetsLarge = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  return result;
+  let arr = str.split("");
+
+  result = arr.map(el => {
+    if (alphabetsSmall.includes(el))
+      return el.charCodeAt(0) - 'a'.charCodeAt(0) +1;
+    else if (alphabetsLarge.includes(el))
+      return el.charCodeAt(0) - 'A'.charCodeAt(0) +1;
+    else  
+      return el;
+  });
+
+  return result.join("");
  }
 
- console.log();
+ console.log(encode('abc')); //'123'
+ console.log(encode('abc-#@5')); //'123-#@5'
+ console.log(encode('ZzzzZ'))
