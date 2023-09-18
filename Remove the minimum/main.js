fac8e46cd25bd8919da11dd24006a60979af7521
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-19 00:30:31 +0500

    [Final] codewars 7Kyu

--- Remove the minimum/main.js
+++ Remove the minimum/main.js
@@ -1,10 +1,25 @@
 function removeSmallest(numbers) {
   let result = [];
 
+  if (numbers.length <=1) 
+    return [];
+  
+  let min = Math.max(...numbers), minIndex= numbers.length;
 
+  numbers.forEach((element, idx) => {
+    if (element < min) {
+      min = element;
+      minIndex = idx;
+    }
+  });
+
+  result = numbers.filter((el, id) => {
+    if (id !== minIndex)
+      return el;
+  })
 
   return result;
 }
 
 console.log(removeSmallest([1, 2, 3, 4, 5])); //[2, 3, 4, 5]
-console.log([2, 2, 1, 2, 1]); //[2, 2, 2, 1]
+console.log(removeSmallest([2, 2, 1, 2, 1])); //[2, 2, 2, 1]
