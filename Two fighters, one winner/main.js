2887e31280ccf14884ea2a548c3cbe3dc7b617f3
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-20 00:46:57 +0500

    codewars 7Kyu

---
+++ Two fighters, one winner/main.js
@@ -0,0 +1,10 @@
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let result = "";
+
+  
+
+  return result;
+}
+
+console.log(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"));
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
