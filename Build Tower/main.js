72326525557b991101c8dd2ca6f452491b6e49c7
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-26 23:39:58 +0500

    [Final] codewars 6Kyu

--- Build Tower/main.js
+++ Build Tower/main.js
@@ -1,7 +1,21 @@
 function towerBuilder(nFloors) {
   let result = [];
 
-  
+  for (let i=1; i<=nFloors; i++) {
+    let temp = '';
+    
+    for (let j=0; j<nFloors-i; j++) {
+      temp += ' ';
+    }
+    for (let j=0; j<2*i-1; j++) {
+      temp += '*';
+    }
+    for (let j=0; j<nFloors-i; j++) {
+      temp += ' ';
+    }
+
+    result.push(temp);
+  }
 
   return result;
 }
