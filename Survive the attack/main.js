cb4c05365073a53251abef991dbe3f740b4b6c99
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-28 23:08:12 +0500

    codewars 7Kyu - not passing all tests

--- Survive the attack/main.js
+++ Survive the attack/main.js
@@ -1,7 +1,24 @@
-function xx(a, b) {
-  let result = false;
+function hasSurvived(a, b) {
+  let result = [];
+  let temp = [];
 
-  return result;
+  a.forEach((element, idx) => {
+    temp.push(b[idx] - element);
+  });
+
+  result = temp.filter(el => el > 0);
+
+  if (result.length == temp.length/2)
+    return a.reduce((acc, c) => acc +c, 0) < b.reduce((acc, c) => acc +c, 0);
+  else if (result.length > temp.length/2)
+    return true;
+  else 
+    return false;  
 }
 
-console.log(xx([ 1, 3, 5, 7 ], [ 2, 4, 6, 8 ]));
+console.log(hasSurvived([2, 9, 9, 7], [1, 1, 3, 8])); //false
+console.log(hasSurvived([ 1, 3, 5, 7 ], [ 2, 4, 6, 8 ])); //true
+console.log(hasSurvived([ 1, 3, 5, 7 ], [ 2, 4 ]));
+console.log(hasSurvived([2, 9, 9, 7], [1, 1, 3, 8])); //false
+console.log(hasSurvived([1, 3, 5, 7], [2, 4, 6, 8])); //true
+console.log(hasSurvived([10, 10, 1, 1], [4, 4, 7, 7])); //true
