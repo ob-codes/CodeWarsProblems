44a7d696c85562d338b95de3c78a33c71e25b8d6
owaisbukhari00 <owaisbukhari@outlook.com>
2023-09-29 12:12:24 +0500

    [Final] codewars 7Kyu

--- Survive the attack/main.js
+++ Survive the attack/main.js
@@ -1,24 +1,34 @@
-function hasSurvived(a, b) {
-  let result = [];
-  let temp = [];
+function hasSurvived(attacker, defender) {
+  let wins = 0, loses = 0;
 
-  a.forEach((element, idx) => {
-    temp.push(b[idx] - element);
-  });
+  let attackPower = attacker.reduce((acc, c) => acc + c, 0);
+  let defendPower = defender.reduce((acc, c) => acc + c, 0);
 
-  result = temp.filter(el => el > 0);
 
-  if (result.length == temp.length/2)
-    return a.reduce((acc, c) => acc +c, 0) < b.reduce((acc, c) => acc +c, 0);
-  else if (result.length > temp.length/2)
+  if (defender.length > attacker.length)
+    wins = defender.length - attacker.length;
+  else
+    loses = attacker.length - defender.length;
+
+  for (let i=0;i< (defender.length > attacker.length ? attacker.length : defender.length); i++) {
+    if (defender[i] > attacker[i])
+      wins++;
+    else if (attacker[i] > defender[i])
+      loses++;
+  }
+
+  if (wins > loses) 
     return true;
+  else if (wins === loses)
+    return defendPower >= attackPower;
   else 
-    return false;  
+    return false;
 }
 
 console.log(hasSurvived([2, 9, 9, 7], [1, 1, 3, 8])); //false
-console.log(hasSurvived([ 1, 3, 5, 7 ], [ 2, 4, 6, 8 ])); //true
-console.log(hasSurvived([ 1, 3, 5, 7 ], [ 2, 4 ]));
+console.log(hasSurvived([1, 3, 5, 7], [2, 4, 6, 8])); //true
+console.log(hasSurvived([1, 3, 5, 7], [2, 4]));
 console.log(hasSurvived([2, 9, 9, 7], [1, 1, 3, 8])); //false
 console.log(hasSurvived([1, 3, 5, 7], [2, 4, 6, 8])); //true
 console.log(hasSurvived([10, 10, 1, 1], [4, 4, 7, 7])); //true
+console.log(hasSurvived([58,14,98,99,3,90,47,70,57,72],[80,80,59,82,66,39,21,94,65,22])); //true
