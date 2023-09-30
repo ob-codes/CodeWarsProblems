157f5017e17590f091dd3946dc5856067e8340fd
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-01 01:43:46 +0500

    [Final] codewars 7Kyu

--- Number of People in the Bus/main.js
+++ Number of People in the Bus/main.js
@@ -1,7 +1,10 @@
-function xx(nums) {
+function number(nums) {
   let onBoarded = 0, offBoarded = 0;
 
-  
+  nums.forEach(element => {
+    onBoarded += element[0];
+    offBoarded += element[1];
+  });
 
   return onBoarded - offBoarded;
 }
