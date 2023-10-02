a46581334ccbb5de72dc1ce6c7c51ca33a33b993
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-02 23:15:17 +0500

    codewars 7Kyu

---
+++ Check the exam/main.js
@@ -0,0 +1,9 @@
+function checkExam(array1, array2) {
+  let result = 0;
+  
+   
+  return result > 0 ? result : 0;
+}
+ 
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"])); //6
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))  //16
