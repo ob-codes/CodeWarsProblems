fc7f133d064456069cffbe756c19d577de7cd558
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-18 23:17:44 +0500

    codewars 7Kyu

---
+++ Categorize New Member/main.js
@@ -0,0 +1,9 @@
+function openOrSenior(data){
+  let result=[];
+
+
+  return result;
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])); //['Open', 'Senior', 'Open', 'Senior']
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]])); //['Open', 'Open', 'Open', 'Open']
