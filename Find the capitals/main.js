aa6cd4319cf411c4f338d4eff72ee4617c39a981
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-23 00:13:31 +0500

    [Final] codewars 7Kyu

--- Find the capitals/main.js
+++ Find the capitals/main.js
@@ -1,6 +1,10 @@
 let capitals = function (word) {
   let result = [];
 
+  word.split("").forEach((el, idx) => {
+    if (el.charCodeAt(0) >= 65 && el.charCodeAt(0) <= 90) 
+      result.push(idx);    
+  });
 
   return result;
 }
