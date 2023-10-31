1b31f39f375800fda31ead47318b18492fd7f584
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-01 00:30:03 +0500

    codewars 7Kyu

---
+++ Get the Middle Character/main.js
@@ -0,0 +1,9 @@
+function getMiddle(s) {
+  let  result = "";
+  
+  return result;
+}
+
+console.log(getMiddle("test")); //es
+console.log(getMiddle("testing")); //t
+console.log(getMiddle("middle")); //dd
