afbf2ce255f54cf028fd714ef31745e7af40eaeb
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-04 12:27:35 +0500

    codewars 6Kyu

---
+++ Duplicate Encoder/main.js
@@ -0,0 +1,10 @@
+function name(params) {
+  let result = "";
+  
+  return result;
+}
+
+console.log(name("din"));//"din"      =>  "((("
+console.log(name("recede"));//"recede"   =>  "()()()"
+console.log(name("Success"));//"Success"  =>  ")())())"
+console.log(name("(( @"));//"(( @"     =>  "))((" 
