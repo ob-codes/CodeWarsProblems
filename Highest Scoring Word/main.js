3693a131abb3f8f3e52817a50c22ea8978092130
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-05 00:58:14 +0500

    [Final] codewars 6Kyu

--- Highest Scoring Word/main.js
+++ Highest Scoring Word/main.js
@@ -2,7 +2,19 @@ function high(x){
   let result="", temp=0, idxOfMaxValue=-1;
   let value=[];
   
+  let arr = x.split(" ");
+
+  arr.forEach(element => {
+    temp = 0;
+    element.split("").forEach(el => {
+      temp += el.charCodeAt(0) - 97 +1;
+    });
+    value.push(temp);
+  });
+
+  idxOfMaxValue = value.indexOf(Math.max(...value)); //finds the index of max value in an array
   
+  return arr[idxOfMaxValue];
 }
 
 console.log(high('man i need a taxi up to ubud'));//'taxi'
