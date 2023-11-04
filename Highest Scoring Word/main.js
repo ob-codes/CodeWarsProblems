e977d96a7dbaa581e241a2f75b2f0803fa715a24
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-05 00:57:57 +0500

    codewars 6Kyu

---
+++ Highest Scoring Word/main.js
@@ -0,0 +1,10 @@
+function high(x){
+  let result="", temp=0, idxOfMaxValue=-1;
+  let value=[];
+  
+  
+}
+
+console.log(high('man i need a taxi up to ubud'));//'taxi'
+console.log(high('what time are we climbing up the volcano'));//'volcano'
+console.log(high('take me to semynak'));//'semynak'
