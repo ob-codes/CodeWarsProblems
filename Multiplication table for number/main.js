c2ed7efdcbd0c620ec03454faa976076c292af95
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-08 22:58:47 +0500

    codewars 8Kyu

---
+++ Multiplication table for number/main.js
@@ -0,0 +1,20 @@
+function multiTable(number) {
+  let result = "";
+  
+  
+  return result.trim();
+}
+
+console.log(multiTable(5));
+/*
+'1 * 5 = 5\n
+2 * 5 = 10\n
+3 * 5 = 15\n
+4 * 5 = 20\n
+5 * 5 = 25\n
+6 * 5 = 30\n
+7 * 5 = 35\n
+8 * 5 = 40\n
+9 * 5 = 45\n
+10 * 5 = 50'
+*/
