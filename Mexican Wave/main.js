a19b009d656478b832a1236166adae8eaa1207c1
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-18 14:31:15 +0500

    [Final] codewars 6Kyu

--- Mexican Wave/main.js
+++ Mexican Wave/main.js
@@ -1,6 +1,11 @@
 function wave(str) {
   let result=[];
   
+  for (let i=0; i<str.length; i++) {
+    if (str[i] === " ")
+      continue;
+    result.push(str.slice(0, i) + str[i].toUpperCase() + str.slice(i+1));
+  }
 
   return result;
 }
