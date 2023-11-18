7c6a08476d71d258e2cf2463b951d383423433b8
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-18 14:30:51 +0500

    codewars 6Kyu

---
+++ Mexican Wave/main.js
@@ -0,0 +1,9 @@
+function wave(str) {
+  let result=[];
+  
+
+  return result;
+}
+
+console.log(wave("hello"));// => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+console.log(wave("two words"));//[ 'Two words', 'tWo words', 'twO words', 'two Words', 'two wOrds', 'two woRds', 'two worDs', 'two wordS' ]
