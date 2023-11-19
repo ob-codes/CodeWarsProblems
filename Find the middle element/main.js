71bd910d606ec73bcd31cef7f9e2457513e3700b
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-19 12:42:52 +0500

    codewars 7Kyu

---
+++ Find the middle element/main.js
@@ -0,0 +1,11 @@
+function gimme(triplet) {
+  let result = -1;
+ 
+  
+  return result;
+}
+
+console.log(gimme([2, 3, 1]));//0
+console.log(gimme([5, 10, 14]));//1
+console.log(gimme([-5.2, -10.6, 14]));//0
+console.log(gimme([5.9, 10.4, 14.2]));//1
