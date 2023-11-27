ef25984235013169b09ad734221e2cef285f925a
owaisbukhari00 <owaisbukhari@outlook.com>
2023-11-27 23:24:55 +0500

    codewars 8Kyu

---
+++ Reversed Strings/main.js
@@ -0,0 +1,8 @@
+function solution(params) {
+  let result = [];
+}
+
+console.log(solution('world')); // =>  'dlrow'
+console.log(solution('hello')); // =>  'olleh'
+console.log(solution('')); // =>  ''
+console.log(solution('h')); // =>  'h'
