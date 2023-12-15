bfc2091a58e75e8b506e0796d35d32e7089aa320
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-15 22:07:59 +0500

    codewars 7Kyu

---
+++ Complementary DNA/main.js
@@ -0,0 +1,19 @@
+function name(params) {
+  let result="";
+
+  params.split("").forEach(el => {
+    if (el === 'A')
+      result+='T';
+    else if (el === 'T')
+      result+='A';
+    else if (el === 'G')
+      result+='C';
+    else if (el === 'C')
+      result+='G';
+  });
+
+  return result;
+}
+
+console.log(name("ATTGC")); //--> "TAACG"
+console.log(name("GTAT")); //--> "CATA"
