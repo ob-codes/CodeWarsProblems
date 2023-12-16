8d22b9c7178828111705805469230ca4fa9bf01d
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-16 10:31:29 +0500

    codewars 8Kyu

---
+++ All Star Code Challenge-18/main.js
@@ -0,0 +1,9 @@
+function str_count(str, ch) {
+  let result = 0;
+
+  return result;
+}
+
+console.log(str_count("Hello", 'o'));//1
+console.log(str_count("Hello", 'l'));//2
+console.log(str_count("", 'z'));//0
