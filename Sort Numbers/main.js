c8794c0438994eedfc00e2ea56f146f549a4419d
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-18 23:29:51 +0500

    commented built in sort()

--- Sort Numbers/main.js
+++ Sort Numbers/main.js
@@ -3,7 +3,7 @@ function solution(params) {
   if (params === null)
     return result;
   
-  result = params.sort((a,b) => a-b);
+  //result = params.sort((a,b) => a-b);
   
   for (let i = params.length -2; i >= 0; i--) {
     if (params[i] > params[i+1]) {
@@ -14,9 +14,9 @@ function solution(params) {
       i = params.length -1;
     }
   }
-  
+
   return params;
 }
 
-console.log(solution([1, 6, 10, 50, 5])); // should return [1,2,5,10,50]
+console.log(solution([1, 6, 10, 50, 5])); // should return [1,2,6,10,50]
 console.log(solution([ 20, 2, 10 ])); // should return []
