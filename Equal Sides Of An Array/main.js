5527141422aec292827ef1cb7d284c839acdf6a7
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-19 20:39:46 +0500

    codewars 6Kyu

---
+++ Equal Sides Of An Array/main.js
@@ -0,0 +1,9 @@
+function namezz(params) {
+  let result = -1;
+  
+  return result;
+}
+
+console.log(namezz([1,2,3,4,3,2,1]));
+console.log(namezz([1,100,50,-51,1,1]));
+console.log(namezz([20,10,-80,10,10,15,35]));
