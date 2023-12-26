26a5cef2379ea126d4fc10ea906e013f6e0ba669
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-26 23:07:24 +0500

    codewars 7kyu

---
+++ Sum of Minimums!/main.js
@@ -0,0 +1,10 @@
+function sumOfMinimums(arr) {
+  let result = 0, temp;
+
+
+  return result;
+}
+
+console.log(sumOfMinimums([ [ 1, 2, 3, 4, 5 ], [ 5, 6, 7, 8, 9 ], [ 20, 21, 34, 56, 100 ]]));//26
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));//9
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]));//76
