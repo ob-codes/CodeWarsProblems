9e5c417ee59892964d4f322e3aa886282210f3c5
owaisbukhari00 <owaisbukhari@outlook.com>
2023-12-29 00:18:43 +0500

    codewars 6Kyu

---
+++ Tortoise racing/main.js
@@ -0,0 +1,10 @@
+function race(a, b, g) {
+  let result = [-1, -1, -1];
+
+  
+  return result;
+}
+
+console.log(race(720, 850, 70));//[0, 32, 18]
+console.log(race(80, 91, 37));//[3, 21, 49]
+console.log(race(80, 100, 40));//[2, 0, 0]
