70f538d365af8479537397c4a78b2eaf5f8acd4d
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-04 00:01:48 +0500

    codewars 6Kyu

---
+++ Convert string to camel case/main.js
@@ -0,0 +1,11 @@
+function toCamelCase(params) {
+  let result = "", paramsArr=[], capitalIndex=-1;
+  
+  
+
+  return result;
+}
+
+console.log(toCamelCase("the-stealth-warrior"));//"theStealthWarrior"
+console.log(toCamelCase("The_Stealth_Warrior"));//"TheStealthWarrior"
+console.log(toCamelCase("The_Stealth-Warrior"));//"TheStealthWarrior"
