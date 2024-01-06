f23b2bf7882726b248fb1d73fe134f0b360154b4
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-06 15:25:19 +0500

    codewars 5Kyu

---
+++ RGB To Hex Conversion/main.js
@@ -0,0 +1,17 @@
+/*
+ 1. Params: numbers, 
+ 2. Return: string,
+ 3. Example: (173, 255,  47) => 'ADFF2F'
+ 4. Pseudo-code: keep input numbers in range (0-255) and convert them to 2-digit hex
+*/
+function rgb(r, g, b) {
+  let result = "", temp=0;
+  
+  
+  return result.toUpperCase();
+}
+
+console.log(rgb(300, 255, 255));//'FFFFFF'
+console.log(rgb(173, 255,  47));//'ADFF2F'
+console.log(rgb(  0,   0,   0));//'000000'
+console.log(rgb(  0,   0, -20));//'000000'
