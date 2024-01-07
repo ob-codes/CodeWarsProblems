1d850b8b36f0045472ea99dc1b8f7cdbe6d46eed
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-07 21:13:54 +0500

    codewars 6Kyu

---
+++ Split Strings/main.js
@@ -0,0 +1,17 @@
+/*
+1. Parameter: string
+2. Return: array of strings
+3. Example: 'abc' =>  ['ab', 'c_'] and 'abcdef' => ['ab', 'cd', 'ef']
+4. Pseudo-code: Split input string in pairs, if last element is single then add '_'
+*/
+
+function solution(params) {
+  let result = [];
+  let argsArr = params.split("");
+
+
+  return result;
+}
+
+console.log(solution('abc'));//=>  ['ab', 'c_']
+console.log(solution('abcdef'));//=> ['ab', 'cd', 'ef']
