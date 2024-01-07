4a15231fc1df7f85bc053eefdac50dd18d3fb997
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-08 00:52:27 +0500

    added decimal to hh:mm:sec time calculation

--- Tortoise racing/main.js
+++ Tortoise racing/main.js
@@ -1,6 +1,19 @@
 function race(a, b, g) {
   let result = [-1, -1, -1];
 
+  temp = g/(b-a);
+
+  if (temp>0) {
+    result = [];
+    temp *= 3600;
+    hr = parseInt(temp / 3600);
+    min = Math.round(parseInt(temp / 60) - (hr*60));
+    sec = Math.round(parseInt(temp) - (min*60) - (hr*3600));
+    
+    result.push(hr);
+    result.push(min);
+    result.push(sec);
+  }
   
   return result;
 }
@@ -8,3 +21,4 @@ function race(a, b, g) {
 console.log(race(720, 850, 70));//[0, 32, 18]
 console.log(race(80, 91, 37));//[3, 21, 49]
 console.log(race(80, 100, 40));//[2, 0, 0]
+console.log(race(720, 850, 37));//[0, 17, 4]
