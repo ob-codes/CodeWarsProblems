c3f97e3b14e4277b2c9b278bd4988a99e5d80e3a
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-09 00:41:03 +0500

    codewars 7Kyu

---
+++ Char Code Calculation/main.js
@@ -0,0 +1,22 @@
+/*
+1. Parameters: string
+2. Return: number
+3. Example: 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667 --> 656661 => Answer(656667 - 656661)
+4. Pseudo-code: 
+  a. Replace char with ASCII code
+  b. Make a string by combining ASCII codes
+  c. Take sum of all digits in ASCII
+  d. Replace number-7 with number-1 in combine ASCII, and Take sum of all digits again
+  e. Calculate difference of original and modified ASCII sum
+*/
+function calc(params) {
+  let result = 0, total1="";
+
+  
+  
+  return result;
+}
+
+console.log(calc('ABC'));
+console.log(calc('aaaaaddddr'));
+console.log(calc('ABC'));
