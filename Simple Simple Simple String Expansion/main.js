67b3bf99ffc3423767777e012d26f3e27ed801b9
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-10 00:09:20 +0500

    [Final] codewars 6Kyu

--- Simple Simple Simple String Expansion/main.js
+++ Simple Simple Simple String Expansion/main.js
@@ -1,4 +1,4 @@
-/*
+/* 
 1. Paramter: string
 2. Return: string
 3. Example: "3abc" -->  "aaabbbccc", 3D2a5d2f" -->  "DDDaadddddff"
@@ -6,11 +6,23 @@
   a. Check for occurence of a number in input string and call it num
   b. For every char that appear after num, repeat the char num times 
 */
-
+//codewars 6Kyu
 function stringExpansion(params) {
   let result="", isPrevNumber = false, num=1;
   paramsArr = params.split("");
   
+  for (let i = 0; i < paramsArr.length; i++) {    
+    let temp = Number(paramsArr[i]);
+    if (temp >= 0 && temp<=9) {
+      isPrevNumber = true;
+      num = temp;
+    }
+    else {      
+      for (let j = 0; j < num; j++) {
+        result += paramsArr[i];
+      }
+    }
+  }
 
   return result;
 }
