f2ec0950b757c1739fae96473d7e53f33c501e68
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-19 21:04:21 +0500

    codewars 5Kyu - added Pseudo code

--- Human Readable Time/main.js
+++ Human Readable Time/main.js
@@ -1,4 +1,13 @@
-
+/*
+1. Parameter: seconds (number)
+2. Return: time in HH:MM:SS format
+3. Example: 3600 = > 01:00:00, 0 => 00:00:00
+4. Pseudo-code:
+  a. Convert input seconds into hours
+  b. Convert residual seconds in minutes, 
+  c. Keep residual seconds as is
+  d. Combine hr, min and secs in HH:MM:SS format
+*/
 //codewars (5Kyu)
 function humanReadable(seconds) {
   let result = "", arr=[];
