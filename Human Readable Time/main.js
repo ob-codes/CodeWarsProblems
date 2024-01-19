3ad670a9dd0642c0cd210af732efa171c3dc7e1a
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-19 21:03:53 +0500

    codewars 5Kyu

---
+++ Human Readable Time/main.js
@@ -0,0 +1,11 @@
+
+//codewars (5Kyu)
+function humanReadable(seconds) {
+  let result = "", arr=[];
+
+
+  return result;
+}
+console.log(humanReadable(359999));//=> 99:59:59
+console.log(humanReadable(3600));//=> 01:00:00
+console.log(humanReadable(0));//=> 00:00:00
