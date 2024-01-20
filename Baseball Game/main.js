9369495494d8bcc28bfb60ff811e1d39c715d9f1
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-20 21:00:41 +0500

    Job Interview Question

---
+++ Baseball Game/main.js
@@ -0,0 +1,19 @@
+/*
+1. Parameter:array of chars
+2. Return: boolean
+3. Example:["5", "2", "C", "D", "+"] => 30
+4. Pseudo-code:
+  a. Take input array and check value of each char
+  b. If value is 0-9, keep it as it is
+  c. If value is 'C', discard last value
+  d. If value if 'D', double the last value
+  e. If value if '+', take the sum of last two values
+  f. Find the sum of all values
+*/
+function name(ops) {
+    var result = null, temp=[];
+
+    return result;
+}
+
+console.log(name(["5", "2", "C", "D", "+"])) //30
