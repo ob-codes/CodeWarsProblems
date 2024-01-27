abedb08c4cea15969c98d093d5eb302e025188c4
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-27 23:57:17 +0500

    [Final] codewars 6Kyu

--- Create Phone Number/main.js
+++ Create Phone Number/main.js
@@ -1,6 +1,19 @@
 function createPhoneNumber(params) {
   let result= "";
 
+  result += '(';
+  result += params[0];
+  result += params[1];
+  result += params[2];
+  result += ') ';
+  result += params[3];
+  result += params[4];
+  result += params[5];
+  result += '-';
+  result += params[6];
+  result += params[7];
+  result += params[8];
+
   return result;
 }
 
