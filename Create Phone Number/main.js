09eba579649073d1f3d24c2fc886492c0d808056
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-28 00:15:56 +0500

    [refactored] codewars 6Kyu

--- Create Phone Number/main.js
+++ Create Phone Number/main.js
@@ -1,20 +1,29 @@
+/*
+1. Params: array of numbers
+2. Return: string
+3. Example: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] => "(123) 456-7890";
+4. Pseudo-code:
+a. Put '(' at the start and keep 1st three digits 
+b. Put ') ' right before 4th digit & keep next three digits
+d. Insert '-' right before 7th digit & keep the remaining digits
+*/
+//codewars (6Kyu)
 function createPhoneNumber(params) {
   let result= "";
+  
+  for (let i = 0; i < params.length; i++) {
+    if (i === 0) 
+      result += '(';
+    else if (i === 3) 
+      result += ') ';
+    else if (i === 6) 
+      result += '-';
 
-  result += '(';
-  result += params[0];
-  result += params[1];
-  result += params[2];
-  result += ') ';
-  result += params[3];
-  result += params[4];
-  result += params[5];
-  result += '-';
-  result += params[6];
-  result += params[7];
-  result += params[8];
+    result += params[i];    
+  }
 
   return result;
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));//"(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));//"(111) 111-1111"
