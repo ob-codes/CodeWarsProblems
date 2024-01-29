408b25c5950bc17b6b1e3e04c9268b09363abdfc
owaisbukhari00 <owaisbukhari@outlook.com>
2024-01-30 01:31:32 +0500

    codewars 6Kyu - recursion

---
+++ Sum of Digits - Digital Root/main.js
@@ -0,0 +1,9 @@
+function digitalRoot(params) {  
+  
+}
+
+console.log(digitalRoot(16));//7
+console.log(digitalRoot(456));//6
+console.log(digitalRoot(567));//9
+console.log(digitalRoot(132189));//6
+  
