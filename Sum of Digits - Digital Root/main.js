26cdbdc9c2a613c41b229efb43eb862dee601d13
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-01 23:59:59 +0500

    added PREP to code

--- Sum of Digits - Digital Root/main.js
+++ Sum of Digits - Digital Root/main.js
@@ -1,5 +1,19 @@
+/*
+1. Parameter: number
+2. Return: number
+3. Example: 456 -> 4+5+6 -> 15 -> 1+5 => 6
+4. Pseudo-code:
+  a. Find the sum of all digits
+  b. If the sum is 2 or more digit value, repeat a
+  c. If the sum is 1 digit value, return the sum as Number
+*/
+//codewars (6Kyu)
 function digitalRoot(params) {  
-  temp = params.toString().split("").reduce((acc, c) => acc + (+c), 0).toString();
+  temp = params
+        .toString()
+        .split("")
+        .reduce((acc, c) => acc + (+c), 0)
+        .toString();
   
   if (params.length <= 1)
     return Number(temp);
@@ -11,4 +25,3 @@ console.log(digitalRoot(16));//7
 console.log(digitalRoot(456));//6
 console.log(digitalRoot(567));//9
 console.log(digitalRoot(132189));//6
-  
