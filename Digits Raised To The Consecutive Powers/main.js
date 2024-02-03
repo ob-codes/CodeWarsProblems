91e61210794532708226087527862a9a1251e87d
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-03 12:03:32 +0500

    codewars 6Kyu - arrays

---
+++ Digits Raised To The Consecutive Powers/main.js
@@ -0,0 +1,15 @@
+/*
+- Parameter: number range
+- Return: array of eurekas
+- Example: 89 -> 8^1+9^2 -> 89 => eureka
+- Pseudo-code:
+*/
+//codewars (6Kyu)
+function raisedSum(a, b) {
+  let result=[], temp=0;
+ 
+  return result;
+}
+
+console.log(raisedSum(90, 100));// --> []
+console.log(raisedSum(1, 100));// --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
