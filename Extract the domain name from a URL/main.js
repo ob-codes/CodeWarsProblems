2c8ac37cfeca74502100c2e27ac4dcc665ddb7d2
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-05 22:03:37 +0500

    codewars 5Kyu- strings

---
+++ Extract the domain name from a URL/main.js
@@ -0,0 +1,14 @@
+/*
+*/
+//codewars (5Kyu)
+function domainName(url) {
+  let result = "";
+  
+  return result;
+}
+
+console.log(domainName("http://github.com/carbonfive/raygun"));//-> domain name = "github"
+console.log(domainName("http://www.zombie-bites.com"));//-> domain name = "zombie-bites"
+console.log(domainName("https://www.cnet.com"));//-> domain name = cnet
+console.log(domainName("anoy1f1lf5y-7vget.fr/default.html"));//-> domain name = anoy1f1lf5y-7vget
+console.log(domainName("www.xakep.ru"));//"xakep"
