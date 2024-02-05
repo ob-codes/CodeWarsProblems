f18bb25b96ddadf87a65fc2b13c1b989d4d45745
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-06 00:44:06 +0500

    codewars 7Kyu - algorithm

---
+++ Growth of a Population/main.js
@@ -0,0 +1,10 @@
+/*
+*/
+//codewars (7Kyu)
+function nbYear(p0, percent, aug, p){
+  let result = 0, years =0;
+  return result;
+}
+
+console.log(nbYear(1500, 5, 100, 5000));//15
+console.log(nbYear(1500000, 2.5, 10000, 2000000));//10
