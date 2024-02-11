6b3d23bbee16c143db8c68afb3483dc04af6f2b9
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-11 17:22:37 +0500

    codewars 6Kyu

---
+++ Stop gninnipS My sdroW!/main.js
@@ -0,0 +1,18 @@
+/*
+- Parameter: string
+- Return: string
+- Example: "Hey fellow warriors" --> "Hey wollef sroirraw"
+- Pseudo=code:
+  a.
+  b.
+  c.
+*/
+//codewars (6Kyu)
+function spinWords(params) {
+  let result = [], temp=[];
+
+  return result.join(" ");
+}
+console.log(spinWords("Hey fellow warriors"));//"Hey wollef sroirraw"
+console.log(spinWords("This is a test"));//"This is a test"
+console.log(spinWords("This is another test"));//"This is rehtona test"
