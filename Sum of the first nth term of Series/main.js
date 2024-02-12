0954467b889ec77035635aaefde92802e855c8a8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-12 17:14:28 +0500

    [Final] codewars 7Kyu

--- Sum of the first nth term of Series/main.js
+++ Sum of the first nth term of Series/main.js
@@ -2,6 +2,14 @@
 function SeriesSum(n) {
   let result = 0, temp = 1;
 
+  if (n>0) 
+    result = 1;
+  
+  for (let i =1; i< n;  i++) {
+    temp += 3;
+    result += 1/temp;
+  }
+
   return result.toFixed(2);
 }
 
