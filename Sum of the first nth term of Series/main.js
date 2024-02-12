2e30b7561e22dc4f34e60a9c50e908d43dd7aa58
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-12 17:14:03 +0500

    codewars 7Kyu

---
+++ Sum of the first nth term of Series/main.js
@@ -0,0 +1,11 @@
+//codewars (7Kyu)
+function SeriesSum(n) {
+  let result = 0, temp = 1;
+
+  return result.toFixed(2);
+}
+
+console.log(SeriesSum(0));
+console.log(SeriesSum(1));
+console.log(SeriesSum(2));
+console.log(SeriesSum(5));
