e046e9880747c611b829dcbefd2a89cf8c80313a
owaisbukhari00 <owaisbukhari@outlook.com>
2024-02-20 23:59:30 +0500

    codewars 7Kyu - array

---
+++ Squares sequence/main.js
@@ -0,0 +1,10 @@
+function squares(x, n) {
+  let result = [];
+
+  return result;
+}
+
+console.log(squares(2,5),[2,4,16,256,65536]);   
+console.log(squares(3,3),[3,9,81]);  
+console.log(squares(5,3),[5,25,625]);  
+console.log(squares(10,4),[10,100,10000,100000000]); 
