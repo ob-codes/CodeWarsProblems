d6222b16f6fe3fb19711c9873096d4f3c7a62d12
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-01 00:00:05 +0500

    codewars 5Kyu - arrays

---
+++ Product of consecutive Fib numbers/main.js
@@ -0,0 +1,11 @@
+//codewars (5Kyu)
+function productFib(num) {
+  let result = [];
+  let fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
+
+  
+  return result;
+}
+
+console.log(productFib(714)); //[ 21, 34, 'true' ]
+console.log(productFib(800)); //[ 34, 55, 'false' ]
