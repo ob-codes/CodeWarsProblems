9a20b4491a8e4fcc3b1421f9bd38e3ff75f65637
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-01 00:01:01 +0500

    [Final] codewars 5Kyu

--- Product of consecutive Fib numbers/main.js
+++ Product of consecutive Fib numbers/main.js
@@ -3,6 +3,21 @@ function productFib(num) {
   let result = [];
   let fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
 
+  while (num > (fib[fib.length -1] * fib[fib.length -2])) {
+    fib.push(fib[fib.length -1] + fib[fib.length -2]);
+  }
+
+  for (let i=0; i< fib.length; i++) {
+    let temp = fib[i] * fib[i -1];
+    if(num === temp) {
+      result.push(fib[i -1], fib[i], true);
+      break;
+    }
+    else if (temp > num) {
+      result.push(fib[i -1], fib[i], false);
+      break;
+    }
+  }
   
   return result;
 }
