aa17ef43dd1c91f2b79f24529b4c61bac884bfe8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-02 10:03:58 +0500

    updated logic to improve space optimization

--- Product of consecutive Fib numbers/main.js
+++ Product of consecutive Fib numbers/main.js
@@ -1,26 +1,23 @@
 //codewars (5Kyu)
 function productFib(num) {
   let result = [];
-  let fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
+  let fib = [0, 1];
 
-  while (num > (fib[fib.length -1] * fib[fib.length -2])) {
-    fib.push(fib[fib.length -1] + fib[fib.length -2]);
+  while (!(fib[0] * fib[1] >=num)) {
+    fib[1] = fib[1] + fib[0];
+    fib[0] = fib[1] - fib[0];
   }
 
-  for (let i=0; i< fib.length; i++) {
-    let temp = fib[i] * fib[i -1];
-    if(num === temp) {
-      result.push(fib[i -1], fib[i], true);
-      break;
-    }
-    else if (temp > num) {
-      result.push(fib[i -1], fib[i], false);
-      break;
-    }
+  let temp = fib[0] * fib[1];
+  if(num === temp) {
+    result.push(fib[0], fib[1], true);
+  }
+  else if (temp > num) {
+    result.push(fib[0], fib[1], false);
   }
   
   return result;
 }
 
-console.log(productFib(714)); //[ 21, 34, 'true' ]
-console.log(productFib(800)); //[ 34, 55, 'false' ]
+console.log(productFib(714)); //[ 21, 34, true ]
+console.log(productFib(800)); //[ 34, 55, false ]
