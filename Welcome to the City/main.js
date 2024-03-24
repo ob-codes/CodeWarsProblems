df4287df8236fb839fd2de8ffb354c7dfef867c8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-24 17:47:26 +0500

    codewars 8Kyu - strings

---
+++ Welcome to the City/main.js
@@ -0,0 +1,6 @@
+function sayHello( name, city, state ) {
+  
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));//Hello, John Smith! Welcome to Phoenix, Arizona!
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'));//'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
