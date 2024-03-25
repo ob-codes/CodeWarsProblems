7b429d6f84d52eee6de1b3f20220490a47816425
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-25 17:48:09 +0500

    [Final] codewars 8Kyu

--- Welcome to the City/main.js
+++ Welcome to the City/main.js
@@ -1,5 +1,7 @@
 function sayHello( name, city, state ) {
+  let fullName = name.reduce((cur, acc) => cur + ' ' + acc, '').trim();
   
+  return `Hello, ${fullName}! Welcome to ${city}, ${state}!`
 }
 
 console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));//Hello, John Smith! Welcome to Phoenix, Arizona!
