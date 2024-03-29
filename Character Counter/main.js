b58758e0074b521f6d3f01c19aefaa30b975635c
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-29 20:55:51 +0500

    codewars 7Kyu - arrays

---
+++ Character Counter/main.js
@@ -0,0 +1,9 @@
+function validateWord(input) {
+  let result = true, count = [];
+
+  return result;
+}
+
+console.log(validateWord("abcabc"));//true
+console.log(validateWord("abcabcd"));//false
+console.log(validateWord("123abc!"));//true
