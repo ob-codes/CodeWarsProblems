64a6654f04b83cbf8ccbef81bc6325e3a0518173
owaisbukhari00 <owaisbukhari@outlook.com>
2024-03-29 20:56:25 +0500

    added more testcases

--- Character Counter/main.js
+++ Character Counter/main.js
@@ -7,3 +7,9 @@ function validateWord(input) {
 console.log(validateWord("abcabc"));//true
 console.log(validateWord("abcabcd"));//false
 console.log(validateWord("123abc!"));//true
+console.log(validateWord("abcabc"));//true
+console.log(validateWord("Abcabc"));//true
+console.log(validateWord("abc123"));//true
+console.log(validateWord("abcabcd"));//false
+console.log(validateWord("abc!abc!"));//true
+console.log(validateWord("abc:abc"));//false
