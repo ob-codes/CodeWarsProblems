263262ec7619e014164e6e1fdcae725f2e59a4de
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-01 22:43:48 +0500

    added more testcases

--- Alternate capitalization/main.js
+++ Alternate capitalization/main.js
@@ -5,3 +5,6 @@ function capitalize(params) {
 }
 
 console.log(capitalize("abcdef"));//['AbCdEf', 'aBcDeF']
+console.log(capitalize("codewars"));//['CoDeWaRs', 'cOdEwArS']
+console.log(capitalize("abracadabra"));//['AbRaCaDaBrA', 'aBrAcAdAbRa']
+console.log(capitalize("codewarriors"));//['CoDeWaRrIoRs', 'cOdEwArRiOrS']
