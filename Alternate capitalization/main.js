35ee72168df93c00af802bcebdb7ecafb9b510ca
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-02 22:44:24 +0500

    [Final] codewars 7Kyu

--- Alternate capitalization/main.js
+++ Alternate capitalization/main.js
@@ -1,5 +1,19 @@
 function capitalize(params) {
   let result = [], temp1 = [], temp2 = [];
+  let arr = params.split("");
+
+  arr.filter((el, idx) => {
+    if (idx % 2 === 0) {
+      temp1.push(el.toUpperCase());
+      temp2.push(el.toLowerCase());
+    }
+    else {
+      temp1.push(el.toLowerCase());
+      temp2.push(el.toUpperCase());
+    }
+  });
+
+  result.push(temp1.join(""), temp2.join(""));
 
   return result;
 }
