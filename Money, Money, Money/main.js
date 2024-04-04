36c50bded165a21440479c33bca7ca60b1c857da
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-04 23:38:00 +0500

    added more testcases

--- Money, Money, Money/main.js
+++ Money, Money, Money/main.js
@@ -5,3 +5,5 @@ function calculateYears(principal, interest, tax, d) {
 }
 
 console.log(calculateYears(1000.00, 0.05, 0.18, 1100.00));//3
+console.log(calculateYears(1000, 0.01625, 0.18, 1200));//14
+console.log(calculateYears(1000, 0.05, 0.18, 1000));//0
