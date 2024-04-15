4ff7f2f378de39ab66913416bfa7f58dabb9cd84
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-15 23:54:57 +0500

    [Final] codewars 8Kyu

--- Exclamation marks series #11/main.js
+++ Exclamation marks series #11/main.js
@@ -1,6 +1,25 @@
 function replace(str) {
   let temp = [];
   
+  str.split("").map(el => {
+    switch (el) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+        temp.push('!');
+        break;
+    default:
+      temp.push(el);
+        break;
+    }
+  });
 
   return temp.join("");
 }
