32bb4e552e3f98d559dc10722a4b35295f488b24
owaisbukhari00 <owaisbukhari@outlook.com>
2024-04-15 23:54:16 +0500

    codewars 8Kyu - strings

---
+++ Exclamation marks series #11/main.js
@@ -0,0 +1,10 @@
+function replace(str) {
+  let temp = [];
+  
+
+  return temp.join("");
+}
+
+console.log(replace("Hi!"));//"Hi!!"
+console.log(replace("aeiou"));//"!!!!!"
+console.log(replace("ABCDE"));//"!BCD!"
