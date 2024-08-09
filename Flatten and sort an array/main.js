674055f61c29f764c0bb3836215ba3c641d6767e
owaisbukhari00 <owaisbukhari@outlook.com>
2024-08-10 00:48:17 +0500

    codewars 7Kyu - arrays

---
+++ Flatten and sort an array/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function flattenAndSort(array) {
+}
+
+console.log(flattenAndSort([]), []);
+console.log(flattenAndSort([[], []]), []);
+console.log(flattenAndSort([[], [1]]), [1]);
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
