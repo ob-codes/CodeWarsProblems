d8ff53877e99db19d307cfe9a83552d1e0f03dc5
owaisbukhari00 <owaisbukhari@outlook.com>
2024-08-12 00:32:49 +0500

    codewars 8Kyu - added no argument instantiation

--- Regular Ball Super Ball/main.js
+++ Regular Ball Super Ball/main.js
@@ -1,7 +1,12 @@
 function BallConstructor(type) {
+  if (arguments.length != 0)
+    this.type = type;
+  else
     this.type = `regular`;
 }
 
+let myBall1 = new BallConstructor('heavy');
 let myBall2 = new BallConstructor();
 
+console.log(myBall1.type); //heavy
 console.log(myBall2.type); //regular
