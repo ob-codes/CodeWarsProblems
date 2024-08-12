3ae82cd5c59fc33a8c4156a66e63bfb8583dc3a8
owaisbukhari00 <owaisbukhari@outlook.com>
2024-08-12 23:08:45 +0500

    [Final] codewars 8Kyu - added object using classes

--- Regular Ball Super Ball/main.js
+++ Regular Ball Super Ball/main.js
@@ -1,3 +1,4 @@
+//using Constructor
 function BallConstructor(type) {
   if (arguments.length != 0)
     this.type = type;
@@ -10,3 +11,16 @@ let myBall2 = new BallConstructor();
 
 console.log(myBall1.type); //heavy
 console.log(myBall2.type); //regular
+
+//using classes
+class myBallObject {
+  constructor(type) {
+    this.type = type
+  }
+  tellType(){
+    return this.type;
+  }
+}
+
+let myBall3 = new myBallObject(`sticky`);
+console.log(myBall3.type); //sticky
