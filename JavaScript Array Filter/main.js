ef874b6cc184aa8a7b88a4bdb0940ad407bec585
owaisbukhari00 <owaisbukhari@outlook.com>
2024-08-14 21:25:43 +0500

    [Final] codewars 7Kyu

--- JavaScript Array Filter/main.js
+++ JavaScript Array Filter/main.js
@@ -1,5 +1,5 @@
 function getEvenNumbers(numbersArray){
-  // filter out the odd numbers
+  return numbersArray.filter(el => el%2 == 0);
 }
 
 console.log(getEvenNumbers([1,2,3,6,8,10]), [2,6,8,10]);
